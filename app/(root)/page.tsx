204ec44629a77896b7cf8b82c9ad5e1e558312a1
Virabyan99 <gmparstone99@gmail.com>
2024-12-10 15:14:59 +0400

    Authentication and UI working code

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+//import { ClerkProvider, UserButton } from "@clerk/nextjs"
+
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="head-text text-left">Home</h1>
+    </>
+  )
+}
